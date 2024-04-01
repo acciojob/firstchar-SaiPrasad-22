21f1959d659e1095414359274cb636bb2af959aa
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-01 22:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstChar(text) {
-  // your code here
+
+	let first = {};
+	let n = text.length
+
+	for (let i = 0; i < n; i++) {
+		first = i[0].trim(); 
+	}
+
+	return first;
+	
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
